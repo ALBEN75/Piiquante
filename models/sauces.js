4663aa618cf5c72529ff2f04e067491712b90fea
Alexandre Benaoudia <sofiane.benaoudia75@gmail.com>
2022-04-20 23:26:16 +0200

    déclaration de notre schema pour notre sauce

---
+++ models/sauces.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const sauceSchema = mongoose.Schema ({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    userId: { type: String, required: true },
+});
+
+module.exports = mongoose.model('Sauce', sauceSchema);
