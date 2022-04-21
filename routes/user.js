a592dffb65d274d3f29dbac8eaa2615c91cf82b9
Alexandre Benaoudia <sofiane.benaoudia75@gmail.com>
2022-04-21 18:19:13 +0200

    commentaires du routeur

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router(); // On utilise la méthode express.Router() qui nous permet de créer des routeurs séparés pour chaque route principale de l'application.
 
-const userCtrl = require('../controllers/user');
+const userCtrl = require('../controllers/user');// On importe nos fonctions pour l'inscription et la connexion a l'aide de cette constante.
 
-router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+// Routes individuelles de notre routeur.
+router.post('/signup', userCtrl.signup);// Route pour l'inscription de l'utilisateur.
+router.post('/login', userCtrl.login);// Route pour la connexion de l'utilisateur.
 
-module.exports = router;
+module.exports = router; // On exporte notre router pour pouvoir l'utiliser.
