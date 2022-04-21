5b7ee33d99b854708c0594a09fb7c8cfd15f1957
Alexandre Benaoudia <sofiane.benaoudia75@gmail.com>
2022-04-21 17:34:32 +0200

    commentaire du fichier auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,19 @@
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');// On importe notre package jsonwebtoken pour générer un access token qui permet un échange sécurisé de donnée entre deux parties.
 
+// Ceci nous permettra de verifier l'access token de l'utilisateur.
 module.exports = (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-    const userId = decodedToken.userId;
+    const token = req.headers.authorization.split(' ')[1];// On récupère le token dans le headers d'authorization de la requête. 
+    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');// jwt décode le token puis le vérifie avec la fonction verify à l'aide de la clé secrète.
+    const userId = decodedToken.userId;// On récupere donc le token décodé de l'utilisateur.
+    
+    // Si la demande contient un ID utilisteur on le compare a celui du token, si ils sont différents on génére une erreur.
     if (req.body.userId && req.body.userId !== userId) {
-      throw 'Invalid user ID';
+      throw 'User ID invalide !';
     } else {
-      next();
+      next(); // Sinon dans le cas contraire ils sont identiques et l'utilisateur est authentifié donc on passe à l'éxecution avec la fonction next().
     }
   } catch {
-    res.status(401).json({
-      error: new Error('Invalid request!')
-    });
+    res.status(401).json({ error: new Error ('Requête invalide !') });// On génére une erreur 401 car la requête néccessite une authenfication valide. 
   }
 };
