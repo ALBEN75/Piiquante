e6defe51ca693cebf13792df601e8f975fdf8da8
Alexandre Benaoudia <sofiane.benaoudia75@gmail.com>
2022-04-21 17:56:55 +0200

    commentaires multer-config.js

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,11 +1,13 @@
-const multer = require('multer');
+const multer = require('multer');// On importe le package multer qui nous permet de gérer nos fichiers téléchargés.
 
+// Différents formats d'images qui seront téléchargées.
 const MIME_TYPES = {
   'image/jpg': 'jpg',
   'image/jpeg': 'jpg',
   'image/png': 'png'
 };
 
+// Ceci nous permettra d'enregister les images envoyées par les utilisateurs. On utilise la méthode destination pour définir le dossier où l'image sera enregistrée et la méthode filename pour renommer une image. 
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
     callback(null, 'images');
@@ -17,4 +19,5 @@ const storage = multer.diskStorage({
   }
 });
 
+// On exporte uniquement le fichier image entièrement configuré.
 module.exports = multer({ storage }).single('image');
